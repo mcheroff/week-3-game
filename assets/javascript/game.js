9d55becf443175b9fb19eb438eca3d3eb214ed24
Michael Cheroff <michaelcheroff@Michaels-MacBook-Pro.local>
2016-05-11 17:58:33 -0500

    Week 3 Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Psychic Game</title>
+</head>
+<body>
+<h1>Psychic Game</h1>
+<script>
+
+<a href="javascript: generateRandomLetter();">"Guess what letter I'm thinking of"</a>
+
+
+
+
+
+</script>
+</body>
+</html>
